0ba4c32ab5cc3c8f05bf2bee108af36935222851
Toast <toast@toastberries.xyz>
2025-07-26 20:40:32 +0200

    o ante pona e toki e nasin sitelen lon lipu "install.sh"
    Refine the wording and formatting in install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SRC_DIR="$(cd "$(dirname "$0")" && pwd)"
 TABLE_DIR="$SRC_DIR/fcitx5/table"
 
 do_build() {
-    printf "[*] mi pali e lipu [.dict] tan lipu open... // Building .dict files from source tables...\n"
+    printf "[*] mi pali e lipu \".dict\" tan lipu open... // Building .dict files from source tables...\n"
     mkdir -p "$TABLE_DIR"
     for file in "$SRC_DIR/table_sources"/*; do
         libime_tabledict "$file" "$TABLE_DIR/$(basename "$file" .txt).dict"
@@ -16,12 +16,12 @@ do_build() {
 
 do_install() {
     if [[ ! -d "$TABLE_DIR" ]] || ! compgen -G "$TABLE_DIR"/* > /dev/null; then
-        printf "[✗] pakala la: lipu [.dict] li kama pali ala // Error: .dict files have not been built yet!\n"
+        printf "[✗] pakala la: lipu \".dict\" li kama pali ala // Error: .dict files have not been built yet!\n"
         exit 1
     fi
 
-    # nimi ante "DESTDIR" li lon la: o pilin e ni: ni li ma pi poki pana la: o pana tawa /usr, taso nimi ante "PREFIX" li lon la: o pana tawa ona
-    # If DESTDIR is set, assume packaging environment and install to /usr, unless overwritten with PREFIX
+    # nimi ante "DESTDIR" li lon la: o pilin e ni: ni li ma pi poki pana la: o pana tawa ma "/usr", taso ni li ante kepeken nimi ante "PREFIX"
+    # If DESTDIR is set, assume packaging environment and install to "/usr", unless overwritten with PREFIX
     if [[ -z "${PREFIX:-}" ]]; then
         if [[ -n "${DESTDIR:-}" ]]; then
             PREFIX="/usr"
@@ -37,14 +37,14 @@ do_install() {
     FCITX5_DIR="$INSTALL_DIR/share/fcitx5"
     ICON_DIR="$INSTALL_DIR/share/icons/hicolor/scalable/status"
 
-    printf "[*] mi pana tawa: $INSTALL_DIR // Installing to: $INSTALL_DIR\n"
+    printf "[*] mi pana tawa: \"$INSTALL_DIR\" // Installing to: \"$INSTALL_DIR\"\n"
 
-    printf "[*] mi pana lipu Fcitx5... // Installing fcitx5 files...\n"
+    printf "[*] mi pana lipu \"Fcitx5\"... // Installing Fcitx5 files...\n"
     for dir in "$SRC_DIR/fcitx5"/*; do
         subdirname="$(basename "$dir")"
         for file in "$dir"/*; do
             filename=$(basename "$file")
-            printf "    - $subdirname/$filename --> $FCITX5_DIR/$subdirname/$filename\n"
+            printf "    - \"$subdirname/$filename\" --> \"$FCITX5_DIR/$subdirname/$filename\"\n"
             install -Dm644 "$file" "$FCITX5_DIR/$subdirname/$filename"
         done
     done
@@ -52,7 +52,7 @@ do_install() {
     printf "[*] mi pana lipu sitelen... // Installing icons...\n"
     for file in "$SRC_DIR/icons"/*; do
         filename=$(basename "$file")
-        printf "    - $filename --> $ICON_DIR/fcitx5-$filename\n"
+        printf "    - \"$filename\" --> \"$ICON_DIR/fcitx5-$filename\"\n"
         install -Dm644 "$file" "$ICON_DIR/fcitx5-$filename"
     done
 
@@ -60,7 +60,7 @@ do_install() {
 }
 
 confirm_non_root_install() {
-    printf "[?] sina pali kepeken lawa Root ala! sina wile ala wile pana tawa tomo sina taso? // You are not running as root! Do you wish to install locally only? [y/N] "
+    printf "[?] sina pali kepeken lawa \"Root\" ala! sina wile ala wile pana tawa tomo sina taso? // You are not running as root! Do you wish to install locally only? [y/N] "
     read -r answer
     case "$answer" in
         [Yy]* ) ;;
@@ -71,7 +71,7 @@ confirm_non_root_install() {
 usage() {
     cat >&2 <<EOF
 Kepeken la: $0 [pali-taso|pana-taso|ala]
-    sina o open e mi kepeken nasin Sudo la: mi pana e ilo tawa kulupu ale pi ilo sona ni
+    o open kepeken nasin "Sudo" la: mi pana e ilo tawa jan ale pi ilo sona ni
     nimi ante "DESTDIR" en nimi ante "PREFIX" li lon, ona li sama ilo Autotools
 Usage: $0 [build-only|install-only|all]
     Run as sudo to install system-wide.
